b1b8f881880986f6ead60afd7b48c9c1ae00a4aa
Selahi KORKUT <37619156+selahikorkut@users.noreply.github.com>
2022-05-11 11:08:10 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Receipt from './components/Receipt';
+import { Container } from '@chakra-ui/react'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Container maxW='container.xl' className='app'>
+   <Navbar/>
+   <Products/>
+   <Receipt/>
+   <Footer/>
+   </Container>
+  );
+}
+
+export default App;
